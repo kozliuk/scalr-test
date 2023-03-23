68c7f1086c72bdc6a77b35e9821e93024c671336
Misha Kozliuk <m.kozliuk@scalr.com>
2023-03-23 16:13:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,14 @@ variable "my_key_name_2" {
   type = string
 }
 
+variable "var3" {
+  type = string
+}
+
+variable "TF_VAR_test1" {
+  type = string
+}
+
 
 resource "null_resource" "test" {
   provisioner "local-exec" {
