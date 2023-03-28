084af1ce2cfda237af82dfa3100c9e6d76a184db
Misha Kozliuk <m.kozliuk@scalr.com>
2023-03-28 20:25:28 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,6 @@ variable "my_key_name" {
 }
 
 
-resource "null_resource" "test3" {
-  provisioner "local-exec" {
-    command = "echo 'Hello world ${var.test1}!'"
-  }
-}
-
 resource "null_resource" "test4" {
   provisioner "local-exec" {
     command = "echo 'Hello world ${var.my_key_name}!'"
