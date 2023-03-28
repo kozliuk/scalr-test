45a00a3a6a412b5f65a0f8febbd38415bd8e7675
Misha Kozliuk <m.kozliuk@scalr.com>
2023-03-28 20:10:48 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,22 @@
 variable "my_key_name" {
   type = string
   sensitive = true
+  default = "1"
 }
 
 variable "my_key_name_2" {
   type = string
+  default = "2"
 }
 
 variable "var3" {
   type = string
+  default = "3"
 }
 
 variable "test1" {
   type = string
+  default = "4"
 }
 
 
