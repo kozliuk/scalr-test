4bde5494cf7b0bce55055d5d566d61b793ac4cb7
Misha Kozliuk <m.kozliuk@scalr.com>
2023-05-11 19:44:08 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 variable "my_key_name" {}
 
 resource "google_compute_disk" "default" {
-  provider = "kozliuk"
+  provider = kozliuk
   name  = "test-disk"
   type  = "pd-ssd"
   zone  = "us-central1-a"
