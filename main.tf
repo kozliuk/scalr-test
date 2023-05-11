0dfe059ae769365f4f4de35b55e29312bc020537
Misha Kozliuk <m.kozliuk@scalr.com>
2023-05-11 20:18:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "google_compute_disk" "default" {
   name  = "test-disk"
   type  = "pd-ssd"
   zone  = "us-central1-a"
-  image = "debian-11-bullseye-v20220719"
+  image = "debian-8-jessie-v20170523"
   labels = {
     environment = "dev"
   }
