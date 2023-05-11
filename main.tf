e735182c67d9fe59bc7735a047350aeabbe99d56
Misha Kozliuk <m.kozliuk@scalr.com>
2023-05-11 23:53:27 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,24 +11,3 @@ resource "google_compute_disk" "default" {
   physical_block_size_bytes = 4096
 }
 
-resource "google_compute_disk" "default2" {
-  name  = "test-disk-2"
-  type  = "pd-ssd"
-  zone  = "us-central1-a"
-  image = "base-ubuntu1804-devel"
-  labels = {
-    environment = "dev"
-  }
-  physical_block_size_bytes = 4096
-}
-
-resource "google_compute_disk" "default3" {
-  name  = "test-disk-3"
-  type  = "pd-ssd"
-  zone  = "us-central1-a"
-  image = "base-ubuntu1804-devel"
-  labels = {
-    environment = "dev"
-  }
-  physical_block_size_bytes = 4096
-}
