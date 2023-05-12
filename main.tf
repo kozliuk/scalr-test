82811e2f83cea8a403be0df641ac67f547f36ecc
Misha Kozliuk <m.kozliuk@scalr.com>
2023-05-12 17:49:18 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,18 @@
 variable "my_key_name" {}
 
-resource "google_storage_bucket" "auto-expire" {
-  name          = "no-public-access-bucket"
-  location      = "US"
-  force_destroy = true
+resource "google_sql_database" "database" {
+  name     = "my-database"
+  instance = google_sql_database_instance.instance.name
+}
 
-  public_access_prevention = "enforced"
+# See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
+resource "google_sql_database_instance" "instance" {
+  name             = "my-database-instance"
+  region           = "us-central1"
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection  = "true"
 }
