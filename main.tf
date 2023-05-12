3d8d6b6cda57a5066bd33562fa0c3bb5a51a25eb
Misha Kozliuk <m.kozliuk@scalr.com>
2023-05-12 17:50:36 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,20 @@ resource "google_sql_database_instance" "instance" {
 
   deletion_protection  = "true"
 }
+
+resource "google_sql_database" "database1" {
+  name     = "my-database1"
+  instance = google_sql_database_instance.instance.name
+}
+
+# See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
+resource "google_sql_database_instance" "instance1" {
+  name             = "my-database-instance1"
+  region           = "us-central1"
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection  = "true"
+}
