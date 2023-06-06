912a8db82802ed1acfe88752636f6c7c2e0bd496
Misha Kozliuk <m.kozliuk@scalr.com>
2023-06-06 15:27:37 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,20 +6,20 @@ resource "null_resource" "test" {
   }
 }
 
-resource "null_resource" "misha" {
-  provisioner "local-exec" {
-    command = "echo 'Hello world ${var.my_key_name}!'"
-  }
-}
+# resource "null_resource" "misha" {
+#   provisioner "local-exec" {
+#     command = "echo 'Hello world ${var.my_key_name}!'"
+#   }
+# }
 
-resource "null_resource" "nastya" {
-  provisioner "local-exec" {
-    command = "echo 'Hello world ${var.my_key_name}!'"
-  }
-}
+# resource "null_resource" "nastya" {
+#   provisioner "local-exec" {
+#     command = "echo 'Hello world ${var.my_key_name}!'"
+#   }
+# }
 
-resource "null_resource" "keke" {
-  provisioner "local-exec" {
-    command = "echo 'Hello world ${var.my_key_name}!'"
-  }
-}
+# resource "null_resource" "keke" {
+#   provisioner "local-exec" {
+#     command = "echo 'Hello world ${var.my_key_name}!'"
+#   }
+# }
