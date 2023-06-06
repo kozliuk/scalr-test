450f9a5fbdbc407819010f2667251d03852b06be
Misha Kozliuk <m.kozliuk@scalr.com>
2023-06-06 17:47:10 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,38 @@
 variable "my_key_name" {}
 
-resource "null_resource" "fdsfsdfsdfds" {
+resource "null_resource" "dsafasfasfasfsc" {
   provisioner "local-exec" {
     command = "echo 'Hello world ${var.my_key_name}!'"
   }
 }
 
-resource "null_resource" "dasfasfasfdafa" {
-  provisioner "local-exec" {
-    command = "echo 'Hello world ${var.my_key_name}!'"
-  }
-}
-
-
-resource "null_resource" "dasdasdasda" {
-  provisioner "local-exec" {
-    command = "echo 'Hello world ${var.my_key_name}!'"
-  }
-}
-
-resource "null_resource" "cszzdsafasf" {
-  provisioner "local-exec" {
-    command = "echo 'Hello world ${var.my_key_name}!'"
-  }
-}
-
-resource "null_resource" "zcsdcwdszc" {
+resource "null_resource" "dasfsafasfsadfa" {
   provisioner "local-exec" {
     command = "echo 'Hello world ${var.my_key_name}!'"
   }
 }
 
 
-resource "null_resource" "csdcsdvcvsdv" {
+resource "null_resource" "dasfasfasfasdwewdq" {
+  provisioner "local-exec" {
+    command = "echo 'Hello world ${var.my_key_name}!'"
+  }
+}
+
+resource "null_resource" "cascasxsawrfawe" {
+  provisioner "local-exec" {
+    command = "echo 'Hello world ${var.my_key_name}!'"
+  }
+}
+
+resource "null_resource" "xwcwdfewfewfwxcwxw" {
+  provisioner "local-exec" {
+    command = "echo 'Hello world ${var.my_key_name}!'"
+  }
+}
+
+
+resource "null_resource" "fwfefewfwdxewdwdw" {
   provisioner "local-exec" {
     command = "echo 'Hello world ${var.my_key_name}!'"
   }
