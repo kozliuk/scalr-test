ff9098b89453cca9a7f2f6cbadce3549bbffb42b
Misha Kozliuk <m.kozliuk@scalr.com>
2023-06-08 18:45:20 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 variable "my_key_name" {}
 
-resource "null_resource" "dfsgsdgsdgd" {
+resource "null_resource" "fsdgsgsdggs" {
   provisioner "local-exec" {
     command = "echo 'Hello world ${var.my_key_name}!'"
   }
 }
 
 
-resource "null_resource" "dsgsgdgsd" {
+resource "null_resource" "gsdgsdgggsd" {
   provisioner "local-exec" {
     command = "echo 'Hello world ${var.my_key_name}!'"
   }
 }
 
-resource "null_resource" "dsgsgdgsdgdgsdgsd" {
+resource "null_resource" "asdgfsgsdg" {
   provisioner "local-exec" {
     command = "echo 'Hello world ${var.my_key_name}!'"
   }
