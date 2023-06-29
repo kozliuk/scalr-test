5d3a737f891cfe3b15e4760f1fe3295f61365407
Misha Kozliuk <mykhailo.kozliuk@gmail.com>
2023-06-29 15:06:30 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
 variable "my_key_name" {}
 
+data "external" "my_data" {
+  program = ["python", "-c", "print('hello!')"]
+}
+
 resource "null_resource" "fdsagfsgsddsagfsdgsdgsd" {
   triggers = {
     test = "some-test-value"
   }
   provisioner "local-exec" {
-    command = "echo 'Hello world 2 ${var.my_key_name}!'"
-    working_dir = "/var/log"
-    interpreter = ["bash", "-ex"]
+    attribute = data.external.my_data.result.updated_attribute
   }
 }
 
