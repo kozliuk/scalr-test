7312d93c65fd8b46de2956e62e4649ca04d3773e
Misha Kozliuk <mykhailo.kozliuk@gmail.com>
2023-06-29 14:53:49 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ variable "my_key_name" {}
 
 resource "null_resource" "fdsagfsgsddsagfsdgsdgsd" {
   provisioner "local-exec" {
-    command = "echo 'Hello world ${var.my_key_name}!'"
+    command = "echo 'Hello world 2 ${var.my_key_name}!'"
   }
 }
 
